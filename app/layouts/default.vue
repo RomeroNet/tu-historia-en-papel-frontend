ddfa1c8f91c8a83d037338d7825761949eae7b08
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2026-05-03 02:18:48 +0200

    feat: Add a basic Header component

---
+++ app/layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Header from "~/components/Header.vue";
+
+useHead({
+  title: "Tu Historia en Papel",
+  meta: [
+    {
+      name: "description",
+      content: "Tu Historia en Papel, diseños a medida."
+    },
+  ],
+  htmlAttrs: {
+    lang: "es"
+  }
+})
+</script>
+
+<template>
+  <NuxtRouteAnnouncer />
+  <Header />
+  <slot />
+</template>
+
+<style scoped>
+
+</style>
---
+++ bin/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run --rm -it -v ${PWD}:/app -p 3000:3000 -u 1000:1000 node:25-bookworm bash -c "cd /app && yarn dev"
