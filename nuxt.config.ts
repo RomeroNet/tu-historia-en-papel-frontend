e03330158fa5a13a976318c1230a6a03a08b83d8
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2026-06-29 01:40:54 +0200

    feat(portfolio): prerender page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,7 @@ export default defineNuxtConfig({
     ],
 
     routeRules: {
-        '/': { prerender: true }
+        '/': { prerender: true },
+        '/portfolio/birthday': { prerender: true }
     }
 })
